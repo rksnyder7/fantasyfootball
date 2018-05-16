a3c8b7be5cab4182414b422df962af618392a201
rksnyder7 <rksnyder7@gmail.com>
2018-05-16 10:26:46 +0200

    added switching pages capability

--- src/App.js
+++ src/App.js
@@ -4,10 +4,19 @@ import CardList from './CardList';
 import SearchBox from './SearchBox';
 import RankingsList from './rankingsList';
 import RankHeader from './RankHeader';
-import { defaultSorted, rowStyle, qbRanks, wrRanks, rbRanks, quarterbacks, columns } from './Data';
+import { defaultSorted, rowStyle, teRanks, qbRanks, wrRanks, rbRanks, quarterbacks, columns } from './Data';
 import BootstrapTable from 'react-bootstrap-table-next';
 
+// make def page, and format functions to work for def too 
+// make homepage; links to other pages and routing ability
+// add links to other pages
+// add background pic
+// make header
 
+// for analytical rankings
+// add averages of others to the original database so all data is 
+// then send input to the data.js file and return according to an if statement which determines order of columns array
+// make a function in data.js that will created needed averages and std seperate from other data, just have same name
 
 class App extends Component {
   constructor() {
@@ -15,7 +24,7 @@ class App extends Component {
     this.state = {
       quarterbacks: quarterbacks,
       searchfield: '',
-      route: 'ranking1',
+      route: 'ranking4',
     }
   }
 
@@ -33,26 +42,24 @@ class App extends Component {
   render() {
 
     const { quarterbacks, searchfield, route } = this.state;
-    const filteredQuarterbacks = quarterbacks.filter(quarterback => {
-      return quarterback.name.toLowerCase().includes(searchfield.toLowerCase())
-    })
+
     return (
-      <div>
-        { route === 'ranking1' 
-          ? <div className='tc'>
-              <h1>Quarterback Rankings</h1>
-              <BootstrapTable defaultSorted={defaultSorted} className="check" bordered={ false} keyField='id' rowStyle={rowStyle} data={ qbRanks } columns={ columns } />
-              <h1>test2</h1>
-            </div>
-          : (
-              <div className='tc'>
-                <h1>Fantasy Football</h1>
-              </div>
-            )
+
+      <div className='tc'>
+        <h2>HEADER HOLD</h2>
+
+      {(() => {
+        switch (route) {
+          case 'ranking1': return <BootstrapTable defaultSorted={defaultSorted} keyField='id' rowStyle={rowStyle} data={ qbRanks } columns={ columns } />
+          case 'ranking2': return <BootstrapTable defaultSorted={defaultSorted} keyField='id' rowStyle={rowStyle} data={ rbRanks } columns={ columns } />
+          case 'ranking3': return <BootstrapTable defaultSorted={defaultSorted} keyField='id' rowStyle={rowStyle} data={ wrRanks } columns={ columns } />
+          case 'ranking4': return <BootstrapTable defaultSorted={defaultSorted} keyField='id' rowStyle={rowStyle} data={ teRanks } columns={ columns } />
+          default: return <h1>homepage</h1>
         }
+      })()}
       </div>
-    );
-  }   
+    );  
+  } 
 }
 
 export default App;
@@ -71,3 +78,8 @@ export default App;
                 // <RankHeader name='Player'/>
                 // <RankingsList playerRanks={rbRanks} order={rbOrder} teamSource={rbPlayerTeam}/>
                 // <br/>
+
+                
+    // const filteredQuarterbacks = quarterbacks.filter(quarterback => {
+    //   return quarterback.name.toLowerCase().includes(searchfield.toLowerCase())
+    // })
